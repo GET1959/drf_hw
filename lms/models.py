9534424a529b12912fc36bae0b500da2240f70f8
gennady <topchiev@mail.ru>
2024-05-22 19:34:32 +0300

    added model payment

--- lms/models.py
+++ lms/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-from users.models import NULLABLE
+NULLABLE = {"blank": True, "null": True}
 
 
 class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
     preview = models.ImageField(upload_to="lms/images", **NULLABLE, verbose_name="Просмотр", help_text="Загрузите изображение")
     description = models.TextField(verbose_name="Описание", help_text="Добавьте описание")
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = "курс"
@@ -22,6 +25,9 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to="lms/images", **NULLABLE, verbose_name="Просмотр",
                                 help_text="Загрузите изображение")
     video = models.TextField(**NULLABLE, verbose_name="Ссылка на видео", help_text="Укажите ссылку на видео")
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = "урок"
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
 
 NULLABLE = {"blank": True, "null": True}
 
@@ -20,3 +21,20 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    P_METHOD_CHOICES = [("cash", "cash"), ("transfer", "transfer")]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь")
+    payment_date = models.DateField(auto_now_add=True, verbose_name="дата платежа")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name="оплаченный курс")
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name="оплаченный урок")
+    amount = models.IntegerField(verbose_name="сумма оплаты")
+    payment_method = models.CharField(max_length=150, choices=P_METHOD_CHOICES, verbose_name="способ платежа")
+
+    def __str__(self):
+        return f"Оплата от {self.payment_date}, {self.amount}"
+
+    class Meta:
+        verbose_name = "оплата"
+        verbose_name_plural = "оплата"
